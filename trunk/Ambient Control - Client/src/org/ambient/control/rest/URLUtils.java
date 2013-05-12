46a1689e67e22c074721964ef1b3bccebca77e1d
firefox0816@googlemail.com <firefox0816@googlemail.com@5aeb2fc9-1dc1-c4b3-1b81-60110fb55070>
2013-05-12 23:08:39 +0000

    refactored switch widget. the widget is now useable. except on network lost. then the phone has to be restarted. will be fixed tomorrow

--- trunk/Ambient Control - Client/src/org/ambient/control/rest/URLUtils.java
+++ trunk/Ambient Control - Client/src/org/ambient/control/rest/URLUtils.java
@@ -3,8 +3,9 @@ package org.ambient.control.rest;
 public class URLUtils {
 	
 //	public static final String[] ANDROID_ADT_SERVERS= new String[]{"10.0.2.2:9998","10.0.2.2:9999"};
-//	public static final String[] ANDROID_ADT_SERVERS= new String[]{"rfmbridge:9998","rfmbridge:9999"};
-	public static final String[] ANDROID_ADT_SERVERS= new String[]{"10.0.2.2:9998"};
+	public static final String[] ANDROID_ADT_SERVERS= new String[]{"rfmbridge:9998","rfmbridge:9999"};
+//	public static final String[] ANDROID_ADT_SERVERS= new String[]{"10.0.2.2:9998"};
+//	public static final String[] ANDROID_ADT_SERVERS= new String[]{"192.168.1.44:9998"};
 	
 	public static String getBaseUrl(String hostname){
 		return "http://"+hostname+"/rest";
--- trunk/Ambient Control - Client/src/org/ambient/widgets/RoomSwitchesWidgetProvider.java
+++ trunk/Ambient Control - Client/src/org/ambient/widgets/RoomSwitchesWidgetProvider.java
@@ -9,37 +9,72 @@ import android.util.Log;
 
 
 public class RoomSwitchesWidgetProvider extends AppWidgetProvider {
-	  private static final String LOG = "de.vogella.android.widget.example";
 
-	  @Override
-	  public void onReceive(Context context, Intent intent){
-		  
-		  Intent intentToService = new Intent(context,
-			        UpdateWidgetService.class);
-			    intentToService.putExtras(intent.getExtras());
+	private static final String LOG = "widgetProvider";
 
-			    // Update the widgets via the service
-			    context.startService(intentToService);
-		  super.onReceive(context, intent);
-	  }
-	  
-	  @Override
-	  public void onUpdate(Context context, AppWidgetManager appWidgetManager,
-	      int[] appWidgetIds) {
-		 
-	    Log.w(LOG, "onUpdate method called");
-	    // Get all ids
-	    ComponentName thisWidget = new ComponentName(context,
-	        RoomSwitchesWidgetProvider.class);
-	    int[] allWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget);
 
-	    // Build the intent to call the service
-	    Intent intent = new Intent(context,
-	        UpdateWidgetService.class);
-	    intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, allWidgetIds);
+//	@Override
+//	public void onEnabled(Context context) {
+//		Log.w(LOG, "onenabled method called");
+//		Intent intentToService = new Intent(context, UpdateWidgetService.class);
+//		intentToService.setAction(null);
+//
+//		// Update the widgets via the service
+//		context.startService(intentToService);
+//	}
 
-	    // Update the widgets via the service
-	    context.startService(intent);
-	  }
-	
+
+//	@Override
+//	public void onDeleted(Context context, int[] appWidgetIds) {
+//		Log.w(LOG, "onDeleted method called");
+//		Intent intentToService = new Intent(context, UpdateWidgetService.class);
+//		intentToService.setAction(null);
+//		// Update the widgets via the service
+//		AppWidgetManager appWidgetManager=AppWidgetManager.getInstance(context);
+//		context.stopService(intentToService);
+//	}
+
+
+	@Override
+	public void onDisabled(Context context) {
+		Log.w(LOG, "onDisabled method called");
+		Intent intentToService = new Intent(context, UpdateWidgetService.class);
+		intentToService.setAction(null);
+		// Update the widgets via the service
+		context.stopService(intentToService);
+	}
+
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		Log.w(LOG, "onReceive method called with " + intent.getAction());
+		
+		//Just handle pending intents from Service
+		if (intent.getAction().contains("SWITCH") == true) {
+			Intent intentToService = new Intent(context, UpdateWidgetService.class);
+			intentToService.putExtras(intent.getExtras());
+			intentToService.setAction(intent.getAction());
+			context.startService(intentToService);
+		}
+		super.onReceive(context, intent);
+	}
+
+
+	@Override
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+
+		Log.w(LOG, "onUpdate method called");
+		// Get all ids
+		ComponentName thisWidget = new ComponentName(context, RoomSwitchesWidgetProvider.class);
+		int[] allWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget);
+
+		// Build the intent to call the service
+		Intent intent = new Intent(context, UpdateWidgetService.class);
+		intent.setAction("updateWidget");
+		intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, allWidgetIds);
+
+		// Update the widgets via the service
+		context.startService(intent);
+	}
+
 }
