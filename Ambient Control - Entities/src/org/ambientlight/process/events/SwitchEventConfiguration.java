40891b69826b9127e37099ba839ff54fd6b1a616
firefox0816 <firefox0816@googlemail.com>
2013-06-25 20:22:18 +0000

    eventhandler now supports scenery changes

--- Ambient Control - Entities/src/org/ambientlight/process/events/SwitchEventConfiguration.java
+++ Ambient Control - Entities/src/org/ambientlight/process/events/SwitchEventConfiguration.java
@@ -3,4 +3,29 @@ package org.ambientlight.process.events;
 public class SwitchEventConfiguration extends EventConfiguration {
 
 	public boolean powerState;
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + (powerState ? 1231 : 1237);
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		SwitchEventConfiguration other = (SwitchEventConfiguration) obj;
+		if (powerState != other.powerState)
+			return false;
+		return true;
+	}
+
 }
