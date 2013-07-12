03a4e5880af72ab61086a702934e9b0a45e1f671
firefox0816@googlemail.com <firefox0816@googlemail.com@5aeb2fc9-1dc1-c4b3-1b81-60110fb55070>
2013-07-12 10:57:02 +0000

    simplified rendercontrol handling

--- trunk/Ambient Control - Middleware/src/org/ambientlight/rendering/effects/transitions/Transition.java
+++ trunk/Ambient Control - Middleware/src/org/ambientlight/rendering/effects/transitions/Transition.java
@@ -2,6 +2,8 @@ package org.ambientlight.rendering.effects.transitions;
 
 import org.ambientlight.rendering.effects.RenderingEffect;
 
+
 public interface Transition extends RenderingEffect {
+
 	public boolean isFinished();
 }
