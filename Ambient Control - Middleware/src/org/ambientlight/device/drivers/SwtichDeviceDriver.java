ff11cf42fa960a8fcbc4a0f7bbe0ba1f82aaaeca
firefox0816 <firefox0816@googlemail.com>
2013-09-29 19:52:17 +0000

    added model service layer

--- Ambient Control - Middleware/src/org/ambientlight/device/drivers/SwtichDeviceDriver.java
+++
@@ -1,9 +0,0 @@
-package org.ambientlight.device.drivers;
-
-import java.io.IOException;
-
-public interface SwtichDeviceDriver extends DeviceDriver {
-	
-	public void setConfiguration(SwitchDeviceOverEthernetConfiguration configuration);
-	public void writeData(String type, int housecode,int switchingUnit,boolean state) throws IOException;
-}
--- Ambient Control - Middleware/src/org/ambientlight/rendering/effects/transitions/FadeOutTransition.java
+++
@@ -1,6 +0,0 @@
-package org.ambientlight.rendering.effects.transitions;
-
-
-public interface FadeOutTransition extends Transition{
-
-}
