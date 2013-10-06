a8fd7a8c1da0072fea0abf8d90e819fc73be98a0
firefox0816 <firefox0816@googlemail.com>
2013-10-06 16:58:09 +0000

    colorCorrection, dithering and sunset programm

--- Ambient Control - Middleware/src/org/ambientlight/rendering/effects/transitions/Transition.java
+++
@@ -1,9 +0,0 @@
-package org.ambientlight.rendering.effects.transitions;
-
-import org.ambientlight.rendering.effects.RenderingEffect;
-
-
-public interface Transition extends RenderingEffect {
-
-	public boolean isFinished();
-}
