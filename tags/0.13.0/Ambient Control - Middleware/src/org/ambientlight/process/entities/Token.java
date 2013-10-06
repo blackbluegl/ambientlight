7066fa9d2dec0fdea0a84bb03936284f67f970ae
firefox0816@googlemail.com <firefox0816@googlemail.com@5aeb2fc9-1dc1-c4b3-1b81-60110fb55070>
2013-10-06 16:59:09 +0000

    colorCorrection, dithering and sunset programm

---
+++ tags/0.13.0/Ambient Control - Middleware/src/org/ambientlight/process/entities/Token.java
@@ -0,0 +1,11 @@
+package org.ambientlight.process.entities;
+
+import org.ambientlight.process.handler.DataTypeValidation;
+
+
+public class Token {
+
+	public DataTypeValidation valueType;
+	public Integer nextNodeId;
+	public Object data;
+}
---
+++ tags/0.13.0/Ambient Control - Middleware/src/org/ambientlight/process/handler/event/EventToBooleanHandler.java
@@ -0,0 +1,53 @@
+/*  Copyright 2013 Florian Bornkessel
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
+package org.ambientlight.process.handler.event;
+
+import org.ambientlight.process.entities.Token;
+import org.ambientlight.process.events.AlarmEventConfiguration;
+import org.ambientlight.process.events.SceneryEntryEventConfiguration;
+import org.ambientlight.process.events.SwitchEventConfiguration;
+import org.ambientlight.process.handler.AbstractActionHandler;
+import org.ambientlight.process.handler.DataTypeValidation;
+
+
+/**
+ * @author Florian Bornkessel
+ * 
+ */
+public class EventToBooleanHandler extends AbstractActionHandler {
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.ambientlight.process.handler.AbstractActionHandler#performAction(
+	 * org.ambientlight.process.entities.TokenValueType, java.lang.Object)
+	 */
+	@Override
+	public void performAction(Token token) {
+		if (token.valueType.equals(DataTypeValidation.EVENT)) {
+			if (token.data instanceof SwitchEventConfiguration) {
+				SwitchEventConfiguration event = (SwitchEventConfiguration) token.data;
+				token.data = event.powerState ? 1.0 : 0.0;
+				token.valueType = DataTypeValidation.BOOLEAN;
+			}
+			if (token.data instanceof AlarmEventConfiguration || token.data instanceof SceneryEntryEventConfiguration) {
+				token.data = 1.0;
+				token.valueType = DataTypeValidation.BOOLEAN;
+			}
+		}
+	}
+}
