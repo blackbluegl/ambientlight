77e44b8154db0884a2131ff5ace2026ab5052c1a
firefox0816 <firefox0816@googlemail.com>
2013-12-23 00:12:53 +0000

    refactored entites and event configs. removed top down principle and introduced a feature based aproach with interfaces to reflect the entity world to the real world in a better way. warning do not use this build. use an older tag or if you want to try the climate manager use an commit from yesterday.

---
+++ Ambient Control - Entities/src/org/ambientlight/config/events/BroadcastEvent.java
@@ -0,0 +1,50 @@
+package org.ambientlight.config.events;
+
+import java.io.Serializable;
+
+import org.ambientlight.annotations.AlternativeValues;
+import org.ambientlight.annotations.FieldType;
+import org.ambientlight.annotations.TypeDef;
+import org.ambientlight.annotations.ValueBindingPath;
+import org.codehaus.jackson.annotate.JsonTypeInfo;
+
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY, property = "@class")
+public abstract class BroadcastEvent implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@TypeDef(fieldType = FieldType.STRING)
+	@AlternativeValues(valueBinding = {
+			@ValueBindingPath(forSubClass = "org.ambientlight.process.events.SceneryEntryEventConfiguration", valueBinding = "getSceneryEventGenerator().keySet()"),
+			@ValueBindingPath(forSubClass = "org.ambientlight.process.events.SwitchEventConfiguration", valueBinding = "getSwitchGenerators().keySet()") })
+	public String sourceId;
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((sourceId == null) ? 0 : sourceId.hashCode());
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		BroadcastEvent other = (BroadcastEvent) obj;
+		if (sourceId == null) {
+			if (other.sourceId != null)
+				return false;
+		} else if (!sourceId.equals(other.sourceId))
+			return false;
+		return true;
+	}
+
+}
