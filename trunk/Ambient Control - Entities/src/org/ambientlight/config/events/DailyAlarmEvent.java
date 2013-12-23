09a619b4103e3ef0f3fdd0b24962f6c0eb5c2b6d
firefox0816@googlemail.com <firefox0816@googlemail.com@5aeb2fc9-1dc1-c4b3-1b81-60110fb55070>
2013-12-23 00:12:53 +0000

    refactored entites and event configs. removed top down principle and introduced a feature based aproach with interfaces to reflect the entity world to the real world in a better way. warning do not use this build. use an older tag or if you want to try the climate manager use an commit from yesterday.

---
+++ trunk/Ambient Control - Entities/src/org/ambientlight/config/events/DailyAlarmEvent.java
@@ -0,0 +1,47 @@
+package org.ambientlight.config.events;
+
+public class DailyAlarmEvent extends BroadcastEvent {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public int hour;
+	public int minute;
+
+
+	public DailyAlarmEvent(int hour, int minute, String sourceName) {
+		this.hour = hour;
+		this.minute = minute;
+		super.sourceId = sourceName;
+	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + hour;
+		result = prime * result + minute;
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DailyAlarmEvent other = (DailyAlarmEvent) obj;
+		if (hour != other.hour)
+			return false;
+		if (minute != other.minute)
+			return false;
+		return true;
+	}
+
+}
---
+++ trunk/Ambient Control - Entities/src/org/ambientlight/config/events/SwitchEvent.java
@@ -0,0 +1,51 @@
+package org.ambientlight.config.events;
+
+import org.ambientlight.annotations.FieldType;
+import org.ambientlight.annotations.TypeDef;
+import org.ambientlight.config.room.entities.switches.SwitchType;
+
+
+public class SwitchEvent extends BroadcastEvent {
+
+	private static final long serialVersionUID = 1L;
+
+	@TypeDef(fieldType = FieldType.BOOLEAN)
+	public boolean powerState;
+
+	public SwitchType type;
+
+
+	@Override
+	public String toString() {
+		String value = "Schalter(" + type + "): " + sourceId + " im Zustand: ";
+		return powerState ? value + "ein" : value + "aus";
+	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + (powerState ? 1231 : 1237);
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		SwitchEvent other = (SwitchEvent) obj;
+		if (powerState != other.powerState)
+			return false;
+		if (type != other.type)
+			return false;
+		return true;
+	}
+
+}
