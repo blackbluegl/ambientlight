2c5cca6cb6c568d9d18141e6e9c8909d5d47763b
firefox0816@googlemail.com <firefox0816@googlemail.com@5aeb2fc9-1dc1-c4b3-1b81-60110fb55070>
2014-01-01 22:02:26 +0000

    was mdmoe service - but is not minidmx and does handle different led systems. so it is called led-bridge from now on.

---
+++ trunk/led-bridge/src/datasocket/StripePortMapping.cpp
@@ -0,0 +1,15 @@
+/*
+ * StripePortMapping.cpp
+ *
+ *  Created on: 05.05.2013
+ *      Author: florian
+ */
+
+#include "StripePortMapping.h"
+
+StripePortMapping::StripePortMapping() {
+
+}
+
+StripePortMapping::~StripePortMapping() {
+}
