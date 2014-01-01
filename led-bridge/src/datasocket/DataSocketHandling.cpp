9c30c2fce4f76494812e819ee968785d8cade64f
firefox0816 <firefox0816@googlemail.com>
2014-01-01 22:02:26 +0000

    was mdmoe service - but is not minidmx and does handle different led systems. so it is called led-bridge from now on.

---
+++ led-bridge/src/datasocket/DataSocketHandling.cpp
@@ -0,0 +1,88 @@
+/*
+ * DataSocketHandling.cpp
+ *
+ *  Created on: 28.11.2012
+ *      Author: florian
+ */
+
+#include "DataSocketHandling.h"
+#include "../tmp1812/Tmp1812SPI.h"
+#include "../directspi/DirectSPI.h"
+#include "StripePortMapping.h"
+#include <sys/socket.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <map>
+
+#define TM1812 "tm1812"
+#define DIRECT_SPI "directSpi"
+
+using namespace std;
+
+DataSocketHandling::DataSocketHandling() {
+	// TODO Auto-generated constructor stub
+}
+
+DataSocketHandling::~DataSocketHandling() {
+}
+
+void DataSocketHandling::handleDataRequests(int &workingControlSocket, map<int, StripePortMapping> &stripePortMapping) {
+
+	map<int, StripePortMapping>::iterator p;
+
+	for (p = stripePortMapping.begin(); p != stripePortMapping.end(); p++) {
+		//later this may be needed to send data to the right port!
+		int portNumber = p->first;
+		StripePortMapping mapping = p->second;
+		string protocoll = mapping.protocollType;
+
+		if (strcmp(protocoll.c_str(), TM1812) == 0) {
+			Tmp1812SPI spiDataSend;
+			spiDataSend.setup(portNumber);
+		}
+
+		if (strcmp(protocoll.c_str(), DIRECT_SPI) == 0) {
+			DirectSPI spiDataSend;
+			spiDataSend.setup(portNumber);
+		}
+	}
+
+	do {
+		for (p = stripePortMapping.begin(); p != stripePortMapping.end(); p++) {
+			//later this may be needed to send data to the right port!
+			int portNumber = p->first;
+			StripePortMapping mapping = p->second;
+			int pixelAmount = mapping.pixelAmount;
+			string protocoll = mapping.protocollType;
+
+			unsigned char* data = (unsigned char *) malloc(3 * pixelAmount * sizeof(unsigned char));
+
+			if (readData(workingControlSocket, data, 3 * pixelAmount) <= 0) {
+				return;
+			}
+
+			if (strcmp(protocoll.c_str(), TM1812) == 0) {
+				Tmp1812SPI spiDataSend;
+				spiDataSend.sendData(portNumber, data, 3 * pixelAmount);
+			}
+
+			if (strcmp(protocoll.c_str(), DIRECT_SPI) == 0) {
+				DirectSPI spiDataSend;
+				spiDataSend.sendData(portNumber, data, 3 * pixelAmount);
+			}
+
+			free(data);
+		}
+	} while (1);
+}
+
+ssize_t DataSocketHandling::readData(int sockd, void *vptr, size_t maxlen) {
+	ssize_t rc;
+	rc = recv(sockd, vptr, maxlen, 0);
+	return rc;
+}
+
---
+++ led-bridge/src/directspi/DirectSPI.cpp
@@ -0,0 +1,46 @@
+/*
+ * DirectSPI.cpp
+ *
+ *  Created on: 30.11.2012
+ *      Author: florian
+ */
+
+#include "DirectSPI.h"
+#include <stdint.h>
+#include <errno.h>
+#include <string.h>
+#include <iostream>
+#include <string>
+#include <wiringPiSPI.h>
+#include <wiringPi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+using namespace std;
+
+DirectSPI::DirectSPI() {
+}
+
+DirectSPI::~DirectSPI() {
+}
+
+
+int DirectSPI::setup(int port) {
+	piHiPri(99);
+
+	if (wiringPiSPISetup (port, 2000000) < 0){
+	  fprintf (stderr, "SPI Setup failed: %s\n", strerror (errno));
+	  return -1;
+	}
+	return 0;
+}
+
+
+int DirectSPI::sendData(int channel, unsigned char *buffer, int sizeOfBuffer) {
+
+		if (wiringPiSPIDataRW(channel, buffer, sizeOfBuffer) < 0) {
+			fprintf(stderr, "SPI send failed: %s\n", strerror(errno));
+		}
+	return 0;
+}
