83e235aafe46c22d7d0a5f80256031223258f834
firefox0816 <firefox0816@googlemail.com>
2015-07-26 20:06:26 +0000

    stripe pixel mapping fixed for stripe parts with only one pixel

--- middleware/src/main/java/org/ambientlight/room/entities/lightobject/util/StripeUtil.java
+++ middleware/src/main/java/org/ambientlight/room/entities/lightobject/util/StripeUtil.java
@@ -14,7 +14,12 @@ public class StripeUtil {
 		List<StripePixelMapping> result = new ArrayList<StripePixelMapping>();
 
 		for (StripePart current : stripeParts) {
-			result.addAll(getStripePixelMapping(current));
+			try {
+				result.addAll(getStripePixelMapping(current));
+			} catch (Exception e) {
+				System.out.println("error while trying to map pixels at position: " + stripeParts.indexOf(current));
+				throw e;
+			}
 		}
 
 		return result;
@@ -25,21 +30,27 @@ public class StripeUtil {
 
 		List<StripePixelMapping> result = new ArrayList<StripePixelMapping>();
 
-		Float pixelDifferenceX = (float) ((stripePart.configuration.endXPositionInRoom - stripePart.configuration.startXPositionInRoom) / (stripePart.configuration.pixelAmount - 1));
-		Float pixelDifferenceY = (float) ((stripePart.configuration.endYPositionInRoom - stripePart.configuration.startYPositionInRoom) / (stripePart.configuration.pixelAmount - 1));
+		if (stripePart.configuration.pixelAmount == 1) {
+			result.add(new StripePixelMapping(stripePart.configuration.startXPositionInRoom,
+					stripePart.configuration.startYPositionInRoom, 0, stripePart));
 
-		for (int i = 0; i < stripePart.configuration.pixelAmount; i++) {
-			float xPosition = stripePart.configuration.startXPositionInRoom + (i * pixelDifferenceX);
-			float yPosition = stripePart.configuration.startYPositionInRoom + (i * pixelDifferenceY);
-			result.add(new StripePixelMapping((int) xPosition, (int) yPosition, i, stripePart));
+		} else {
+
+			Float pixelDifferenceX = (float) ((stripePart.configuration.endXPositionInRoom - stripePart.configuration.startXPositionInRoom) / (stripePart.configuration.pixelAmount - 1));
+			Float pixelDifferenceY = (float) ((stripePart.configuration.endYPositionInRoom - stripePart.configuration.startYPositionInRoom) / (stripePart.configuration.pixelAmount - 1));
+
+			for (int i = 0; i < stripePart.configuration.pixelAmount; i++) {
+				float xPosition = stripePart.configuration.startXPositionInRoom + (i * pixelDifferenceX);
+				float yPosition = stripePart.configuration.startYPositionInRoom + (i * pixelDifferenceY);
+				result.add(new StripePixelMapping((int) xPosition, (int) yPosition, i, stripePart));
+			}
 		}
 		return result;
 	}
 
 
 	/**
-	 * determine if a stripePart has pixels that are located within a
-	 * lightObject.
+	 * determine if a stripePart has pixels that are located within a lightObject.
 	 * 
 	 * @param stripePart
 	 * @param lightObjectConfig
@@ -58,9 +69,8 @@ public class StripeUtil {
 		// if pixel is within lightObject the stripe is part of it
 		for (StripePixelMapping currentPixel : pixelMapping) {
 			if (currentPixel.xPosition >= xStart && currentPixel.xPosition <= xEnd && currentPixel.yPosition >= yStart
-					&& currentPixel.yPosition <= yEnd) {
+					&& currentPixel.yPosition <= yEnd)
 				return true;
-			}
 		}
 
 		return false;
